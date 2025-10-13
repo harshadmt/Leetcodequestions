01a2f6f7a57719a43bd6925cc18112ddf5f36ed1
Harshad M T <harshadmuhammad168@gmail.com>
2025-10-13 09:53:07 +0530

    Time: 0 ms (100%), Space: 55.5 MB (11.81%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var balancedStringSplit = function(s) {
+    let balance = 0;
+    let count = 0;
+
+    
+    for (let char of s) {
+        if (char === 'R') {
+            balance++;
+        } else {
+            balance--;
+        }
+
+        if (balance === 0) {
+            count++;
+        }
+    }
+
+    return count;
+};
